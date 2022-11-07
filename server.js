98effa11eb801bb6f78d465616baaceb360ce4a8
scott <fantasticmrscott@gmail.com>
2022-11-07 10:36:01 -0600

    added commands for quotes

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const SerialPort = require("serialport");
 const Readline = require("@serialport/parser-readline");
 var ComfyJS = require("comfy.js");
 require('dotenv').config();
+const fun = require("./functions.js");
+const fs = require('fs');
+const quotes = require("./quotes.js");
 
 
 
@@ -12,46 +15,90 @@ port.pipe(parser);
 
 parser.on("data", (line) => console.log(line));
 
-// write exceptions for NULL colors and alerts
-function hexToRgb(hex) {
-    var hex = hex.replace(/[^0-9A-F]/gi, ''); //remove # from hexcode
-    var bigint = parseInt(hex, 16);
-    var r = (bigint >> 16) & 255;
-    var g = (bigint) >> 8 & 255;
-    var b = bigint & 255;
-    var result =  r.toString().padStart(3, '0') + g.toString().padStart(3, '0') + b.toString().padStart(3,'0'); //make sure RGB is in xxx,xxx,xxx format
-    var result = result + encodeURI('@'); // @ symbold alerts the arduino that the string is complete
-    return result;
+function hasNumbers(test) {
+  var regex = /\d/g;
+  return regex.test(test);
 }
 
+ComfyJS.onCheer = ( user, message, bits, flags, extra ) => {
+  //ComfyJS.Say(user + " just threw " + bits + " bits at Scott! Thanks!");
+  ComfyJS.Say(user + " just threw " + bits + " bits at Scott! Thanks!");
+}
 
+ComfyJS.onSub = (user, message, subTierInfo, extra) => {
+  //ComfyJS.Say(user + " just subscribed at tier " + subTierInfo);
+  if(subTierInfo.plan === "1000") {
+    var tier = "1";
+  } else if(subTierInfo.plan === "2000") {
+    var tier = "2";
+  } else {
+    var tier = "3";
+  }
+  ComfyJS.Say(user + " just subscribed at tier " + tier );
+}
+
+ComfyJS.onResub = (user, message, streamMonths, cumulativeMonths, subTierInfo, extra) => {
+  //ComfyJS.Say(user + " just subscribed! They have been subbed for " + streamMonths + "!");
+  if(subTierInfo.plan === "1000") {
+    var tier = "1";
+  } else if(subTierInfo.plan === "2000") {
+    var tier = "2";
+  } else {
+    var tier = "3";
+  }
+  ComfyJS.Say(user + " just subscribed at tier " + tier + "! They have been subbed for " + cumulativeMonths + " months!");
+}
+
+ComfyJS.onRaid = (user, viewers, extra) => {
+  //ComfyJS.Say(user + " just raided with " + viewers + " viewers! Thank you so much for entrusting me with your viewers!");
+  ComfyJS.Say(user + " just raided with " + viewers + " viewers! Thank you so much for entrusting me with your viewers!");
+}
+
+ComfyJS.onSubGift = (gifterUser, streakMonths, recipientUser, senderCount, subTierInfo, extra) => {
+  //ComfyJS.Say(gifterUser + "just gifted a sub to " + recipientUser + "! Be sure to say thanks!");
+  if(subTierInfo.plan === "1000") {
+    var tier = "1";
+  } else if(subTierInfo.plan === "2000") {
+    var tier = "2";
+  } else {
+    var tier = "3";
+  }
+  ComfyJS.Say(gifterUser + " just gifted a tier " + tier + " sub to " + recipientUser + "! Be sure to say thanks!");
+}
+
+ComfyJS.onCommand = (user, command, message, flags, extra) => {
+  if(command === "addquote") {
+    var date = extra.timestamp;
+    quotes.addQuote(message, date);
+  } else if (command == "quote" && hasNumbers(message)) {
+    var quoteId = JSON.parse(message.match(/\d+/));
+    quotes.getSpecificQuote(user, quoteId - 1);
+  } else if (command === "quote") {
+      quotes.getRandomQuote(user);
+  } else if ((flags.broadcaster || flags.mod) && (command == "deletequote" && hasNumbers(message))) {
+    var quoteId = JSON.parse(message.match(/\d+/));
+    quotes.deleteQuote(quoteId);
+  }
+}
 
 //scan every message and parse user color. write to arduino
 ComfyJS.onChat = (user, message, flags, self, extra ) => {
   console.log(user, extra.userColor);
   if(!extra.userColor) {
-    hex = "#5A5A5A";
-  } else if((user = "fantasticmrscott" || user == process.env.TWITCH_BOT_USERNAME ) && (message.includes("subscribed") || message.includes("raid"))) {
+    hex = "#18160D";
+  } else if((user == process.env.TWITCH_BOT_USERNAME || user == "StreamElements") && (message.includes("gifted") || message.includes("subscribed") || message.includes("raided"))) {
     hex = "#FFFFFF";
-  } else if((user = "fantasticmrscott" || user == process.env.TWITCH_BOT_USERNAME) && (message.includes("follow") || message.includes("cheered"))) {
+  } else if((user == process.env.TWITCH_BOT_USERNAME || user == "StreamElements") && (message.includes("follow") || message.includes("bits"))) {
     hex = "#FFFFFE";
-  } else if(user !== "fantasticdrbot") {
-    var hex = extra.userColor;
   } else {
+    hex = extra.userColor;
   }
-  var result = hexToRgb(hex);
+  var result = fun.hexToRgb(hex);
   console.log(result);
   port.write(result);
 }
 
 
-//just a test command.
-// ComfyJS.onCommand = ( user, command, message, flags, extra ) => {
-//   if( flags.broadcaster && command === "test" ) {
-//     ComfyJS.Say( "!test was typed in chat" );
-//   }
-// }
 
 
-
-ComfyJS.Init( process.env.TWITCH_BOT_USERNAME, process.env.TWITCH_OAUTH_TOKEN, ["FantasticMrScott", "Lekristyyy", "invert180"]);
+ComfyJS.Init( process.env.TWITCH_BOT_USERNAME, process.env.TWITCH_OAUTH_TOKEN, ["fantasticmrscott"]);
